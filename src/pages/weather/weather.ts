6bf0d1099784e4854d2d7fff644efd8177db793d
Luis Manuel Cruza <luismanuel.cruza@gmail.com>
2017-01-07 19:47:54 +0100

    first commit

---
+++ src/pages/weather/weather.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'weather-page',
+    templateUrl: 'weather.html'
+})
+export class WeatherPage implements OnInit {
+    
+    constructor() { }
+
+    ngOnInit() { }
+}
